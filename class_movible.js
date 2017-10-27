148a3b44a8a371e0deee045b4eda4832e438be35
Juan Ruiz <juarui02@ucm.es>
2017-10-27 18:51:14 +0200

    actualizacion script class_movible

--- class_movible.js
+++ class_movible.js
@@ -1,9 +1,19 @@
+//Scripta para objetos movibles
+
 "use strict";
 
+//Constructora
 function movible(velx, vely, dir){
 	this.velocity = {vx: velx, vy: vely};
 	this.direction = dir;
 }
 var obj = require('./class_objeto');
-var object = new obj.objeto(4, 6, "hola");
 
+//Creamos el objeto object para acceder a la posicion
+var object = new obj.objeto(4, 6, " ");
+
+//Metodo para aplicar la velocidad
+movible.prototype.actualiza_pos = function(vx, vy){
+	object.posicion.x = vx*object.posicion.x;
+	object.posicion.y = vy*object.posicion.y;
+};
