6735f7c7bfaab57dcd8e205f3cbca57e6b67e7b4
Javier Cordero Calvo <jacord01@ucm.es>
2017-11-19 17:53:35 +0100

    No funciona absolutamente nada porque no tengo ni la mas remota idea de absolutamente nada no entiendo nada ayuda.

--- Xtreme-SunBlind/src/js/main.js
+++ Xtreme-SunBlind/src/js/main.js
@@ -5,10 +5,11 @@ var PlayScene = require('./play_scene.js');
 
 var BootScene = {
   preload: function () {
-    // load here assets required for the loading screen
-    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+    // Hacemos las cosas necesarias para la primera pantalla de carga
+    //Ahora mismo está vacío porque no tenemos nada que hacer en la pantalla de carga
+   
   },
-
+    //Aquí vamos a llamar a la siguiente función, la de carga anterior al juego
   create: function () {
     this.game.state.start('preloader');
   }
@@ -17,14 +18,20 @@ var BootScene = {
 
 var PreloaderScene = {
   preload: function () {
-    this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
-    this.loadingBar.anchor.setTo(0, 0.5);
-    this.load.setPreloadSprite(this.loadingBar);
+    //Color de fondo
+    //game.stage.backgroundColor = '#f0ff';
 
-    // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
+    //Carga assets?
+     //game.load.baseURL = 'http://examples.phaser.io/assets/';
+     //game.load.crossOrigin = 'anonymous';
+
+    //carganos una imagen
+      this.game.load.image('patata','images/original.png' );
+      this.game.load.image('tostadora', 'images/tostadora.png');
+      console.log('Todo bien');
   },
 
+//Cuando ya hemos cargado todo lo necesario, inciamos el juego en "play_scene.js"
   create: function () {
     this.game.state.start('play');
   }
@@ -32,11 +39,21 @@ var PreloaderScene = {
 
 
 window.onload = function () {
+
+  console.log('primero');
+  //Cuando carga la ventana: Crea la variable juego
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 
+  //Añadimos los estados que vamos a necesitar, en este caso el arranque,
+  //que es para la pantalla de carga, el Preload que es la carga anterior al juego
+  //y finalmente el juego en sí.
   game.state.add('boot', BootScene);
   game.state.add('preloader', PreloaderScene);
   game.state.add('play', PlayScene);
 
+  //Después de crearlas, llamamos al boot
+
   game.state.start('boot');
 };
+
+module.exports = game;
--- Xtreme-SunBlind/src/js/play_scene.js
+++ Xtreme-SunBlind/src/js/play_scene.js
@@ -1,11 +1,62 @@
 'use strict';
 
+var patata;
+var tostadora;
+var cursors;
+var jumpButton;
+
 var PlayScene = {
   create: function () {
-    var logo = this.game.add.sprite(
-      this.game.world.centerX, this.game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
-  }
+
+  	this.stage.backgroundColor = '#124184';
+    patata = this.add.sprite(50, 0, 'patata');
+
+    game.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+    //patata.body.collideWorldBounds = true;
+    //patata.body.gravity.y = 50;
+    patata.scale.setTo(0.5, 0.5);
+
+    tostadora = this.add.physicsGroup();
+
+    //tostadora.create(500, 150, 'tostadora');
+    tostadora.create(0, 1000, 'tostadora');
+    tostadora.create(400, 450, 'tostadora');
+
+    tostadora.setAll('body.immovable', true);
+    tostadora.scale.setTo(1, 0.25);
+
+    cursors = this.input.keyboard.createCursorKeys();
+    jumpButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    update();
+	}
 };
+
+function update(){
+
+	console.log('llega');
+	/* game.physics.arcade.collide(player, platforms);
+
+    patata.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        patata.body.velocity.x = -250;
+    }
+    else if (cursors.right.isDown)
+    {
+        patata.body.velocity.x = 250;
+    }
+
+    if (jumpButton.isDown && (patata.body.onFloor() || patata.body.touching.down))
+    {
+        patata.body.velocity.y = -400;
+    }*/
+ 
+}
+
+
+
 
 module.exports = PlayScene;
