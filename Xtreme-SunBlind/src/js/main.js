eedfb62a675900cbe4d85c64dceec994fa386cec
Javier Cordero Calvo <jacord01@ucm.es>
2017-11-21 19:51:34 +0100

    Una patata salta a través de tostadoras sin herencia alguna

--- Xtreme-SunBlind/src/js/main.js
+++ Xtreme-SunBlind/src/js/main.js
@@ -1,6 +1,100 @@
 'use strict';
 
-var PlayScene = require('./play_scene.js');
+/**
+ * Generated from the Phaser Sandbox
+ *
+ * //phaser.io/sandbox/ljYYfSmV
+ *
+ * This source requires Phaser 2.6.2
+ */
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+
+
+function preload() {
+
+    game.stage.backgroundColor = '#1ff';
+
+   //game.load.baseURL = 'http://examples.phaser.io/assets/';
+    //game.load.crossOrigin = 'anonymous';
+
+    game.load.image('player', 'images/original.png');
+    game.load.image('platform', 'images/tostadora.png');
+
+}
+
+var player;
+var platforms;
+var cursors;
+var jumpButton;
+
+function create() {
+
+    player = game.add.sprite(250, 500, 'player');
+    player.scale.setTo(0.2, 0.2) ;
+
+    game.physics.arcade.enable(player);
+
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 400;
+
+    platforms = game.add.physicsGroup();
+    platforms.scale.setTo(1, 0.1);
+
+   
+    platforms.create(-300, 4000, 'platform');
+    platforms.create(600, 4000, 'platform');
+
+   
+    platforms.create(-450, 2800, 'platform');
+    platforms.create(750, 2800, 'platform');
+
+    platforms.create(150, 2500, 'platform');
+
+    platforms.create(-150, 1000, 'platform');
+    platforms.create(500, 1000, 'platform');
+
+    platforms.create(0,5500, 'platform');
+    platforms.create(500,5500, 'platform');
+
+
+    platforms.setAll('body.immovable', true);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+}
+
+function update () {
+
+  //console.log(player.position);
+    game.physics.arcade.collide(player, platforms);
+
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -250;
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 250;
+    }
+
+    if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down))
+    {
+        player.body.velocity.y = -400;
+    }
+}
+
+function render () {
+
+}
+
+
+
+
+/*var PlayScene = require('./play_scene.js');
 
 
 var BootScene = {
@@ -17,7 +111,8 @@ var BootScene = {
 
 
 var PreloaderScene = {
-  preload: function () {
+
+  }
     //Color de fondo
     //game.stage.backgroundColor = '#f0ff';
 
@@ -56,4 +151,4 @@ window.onload = function () {
   game.state.start('boot');
 };
 
-module.exports = game;
+module.exports = game;*/
