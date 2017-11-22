b2ebdbd0f8859317aa9f84edee512ed969773e2e
Javier Cordero Calvo <jacord01@ucm.es>
2017-11-22 13:00:13 +0100

    La patata se mueve por la pantalla y si pulsas ESPACIO se reescala

--- Xtreme-SunBlind/src/js/main.js
+++ Xtreme-SunBlind/src/js/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * Generated from the Phaser Sandbox
- *
- * //phaser.io/sandbox/ljYYfSmV
- *
- * This source requires Phaser 2.6.2
- */
-
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
 
@@ -15,76 +7,42 @@ function preload() {
 
     game.stage.backgroundColor = '#1ff';
 
-   //game.load.baseURL = 'http://examples.phaser.io/assets/';
-    //game.load.crossOrigin = 'anonymous';
-
     game.load.image('player', 'images/original.png');
-    game.load.image('platform', 'images/tostadora.png');
+    game.load.image('tostadora', 'images/tostadora.png');
 
 }
 
-var player;
+var jugador;
 var platforms;
 var cursors;
 var jumpButton;
 
 function create() {
 
-    player = game.add.sprite(250, 500, 'player');
-    player.scale.setTo(0.2, 0.2) ;
+    jugador = new objeto( 0,0,'player');
 
-    game.physics.arcade.enable(player);
-
-    player.body.collideWorldBounds = true;
-    player.body.gravity.y = 400;
-
-    platforms = game.add.physicsGroup();
-    platforms.scale.setTo(1, 0.1);
-
-   
-    platforms.create(-300, 4000, 'platform');
-    platforms.create(600, 4000, 'platform');
-
-   
-    platforms.create(-450, 2800, 'platform');
-    platforms.create(750, 2800, 'platform');
-
-    platforms.create(150, 2500, 'platform');
-
-    platforms.create(-150, 1000, 'platform');
-    platforms.create(500, 1000, 'platform');
-
-    platforms.create(0,5500, 'platform');
-    platforms.create(500,5500, 'platform');
-
-
-    platforms.setAll('body.immovable', true);
-
-    cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    console.log(jugador);
 
 }
 
 function update () {
 
-  //console.log(player.position);
-    game.physics.arcade.collide(player, platforms);
 
-    player.body.velocity.x = 0;
+    if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+      jugador.cambia_pos(jugador.posicion.x - 10, jugador.posicion.y);
 
-    if (cursors.left.isDown)
-    {
-        player.body.velocity.x = -250;
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.velocity.x = 250;
-    }
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+       jugador.cambia_pos(jugador.posicion.x + 10, jugador.posicion.y);
 
-    if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down))
-    {
-        player.body.velocity.y = -400;
-    }
+    if(game.input.keyboard.isDown(Phaser.Keyboard.UP))
+       jugador.cambia_pos(jugador.posicion.x, jugador.posicion.y - 10);
+
+     else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+       jugador.cambia_pos(jugador.posicion.x, jugador.posicion.y + 10);
+
+     if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+       jugador.reescala_imagen(0.5,0.5);
+
 }
 
 function render () {
@@ -92,63 +50,56 @@ function render () {
 }
 
 
+//****************FUNCION OBJETO******************\\
 
 
-/*var PlayScene = require('./play_scene.js');
+function objeto(entradax, entraday, entradasprite){
 
 
-var BootScene = {
-  preload: function () {
-    // Hacemos las cosas necesarias para la primera pantalla de carga
-    //Ahora mismo está vacío porque no tenemos nada que hacer en la pantalla de carga
-   
-  },
-    //Aquí vamos a llamar a la siguiente función, la de carga anterior al juego
-  create: function () {
-    this.game.state.start('preloader');
-  }
+  this.posicion = {x: entradax, y: entraday};
+  this.sprite = entradasprite;
+  this.objeto = game.add.sprite(this.posicion.x,this.posicion.y,this.sprite);
+
+}
+
+//Metodo para cambiar la posicion del objeto
+objeto.prototype.cambia_pos = function(newposx, newposy){
+  
+  this.objeto.x = newposx;
+  this.objeto.y = newposy;
+  this.posicion.x =  newposx;
+  this.posicion.y = newposy;
+ 
+ 
+
 };
 
+//Metodo para cambiar el sprite del objeto
+objeto.prototype.cambia_sprite = function (newsprite) {
 
-var PreloaderScene = {
-
-  }
-    //Color de fondo
-    //game.stage.backgroundColor = '#f0ff';
-
-    //Carga assets?
-     //game.load.baseURL = 'http://examples.phaser.io/assets/';
-     //game.load.crossOrigin = 'anonymous';
-
-    //carganos una imagen
-      this.game.load.image('patata','images/original.png' );
-      this.game.load.image('tostadora', 'images/tostadora.png');
-      console.log('Todo bien');
-  },
-
-//Cuando ya hemos cargado todo lo necesario, inciamos el juego en "play_scene.js"
-  create: function () {
-    this.game.state.start('play');
-  }
+  this.objeto.sprite = newsprite;
 };
 
+objeto.prototype.reescala_imagen = function (x, y){
 
-window.onload = function () {
+  this.objeto.scale.setTo(x,y);
+}
 
-  console.log('primero');
-  //Cuando carga la ventana: Crea la variable juego
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 
-  //Añadimos los estados que vamos a necesitar, en este caso el arranque,
-  //que es para la pantalla de carga, el Preload que es la carga anterior al juego
-  //y finalmente el juego en sí.
-  game.state.add('boot', BootScene);
-  game.state.add('preloader', PreloaderScene);
-  game.state.add('play', PlayScene);
+//*************FUNCION MOVIBLE**************************\\
 
-  //Después de crearlas, llamamos al boot
+function movible(velx, vely, dir){
+  this.velocity = {vx: velx, vy: vely};
+  this.direction = dir;
+}
 
-  game.state.start('boot');
+//Creamos el objeto object para acceder a la posicion
+
+//Metodo para aplicar la velocidad
+movible.prototype.actualiza_pos = function(vx, vy){
+  object.posicion.x = vx*object.posicion.x;
+  object.posicion.y = vy*object.posicion.y;
 };
 
-module.exports = game;*/
+//***************************************************************\\
+
