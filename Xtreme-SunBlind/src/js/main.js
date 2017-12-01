0eac45adced266d57641160a070903da0bea2ba7
Javier Cordero Calvo <jacord01@ucm.es>
2017-12-01 20:28:17 +0100

    Vuelta a la normalidad en el repo

--- Xtreme-SunBlind/src/js/main.js
+++ Xtreme-SunBlind/src/js/main.js
@@ -5,10 +5,7 @@ var PlayScene = require('./play_scene.js');
 var BootScene = {
   preload: function () {
     // load here assets required for the loading screen
-	this.game.load.baseURL = 'https://Jacord01.io/Xtreme/src/';
-	this.game.load.crossOrigin = 'anonymous';
-
-    //this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
   },
 
   create: function () {
@@ -23,7 +20,6 @@ var PreloaderScene = {
     this.loadingBar.anchor.setTo(0, 0.5);
     this.load.setPreloadSprite(this.loadingBar);
 
-    //Cargamos todas las imagenes
     // TODO: load here the assets for the game
     this.game.load.image('logo', 'images/phaser.png');
     this.game.stage.backgroundColor = '#1ff';
