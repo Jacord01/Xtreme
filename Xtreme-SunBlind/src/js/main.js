8cdb83700576328572fbdebc363952ebdc80c5b4
Javier Cordero Calvo <jacord01@ucm.es>
2017-12-08 14:31:02 +0100

    Intento 3

--- Xtreme-SunBlind/src/js/main.js
+++ Xtreme-SunBlind/src/js/main.js
@@ -5,7 +5,7 @@ var PlayScene = require('./play_scene.js');
 var BootScene = {
   preload: function () {
     // load here assets required for the loading screen
-    this.game.load.baseURL = 'https://Jacord01.io/Jacord01/Xtreme/tree/gh-pages/Xtreme-SunBlind/src/';
+    this.game.load.baseURL = 'https://Jacord01.io/Jacord01/Xtreme/tree/gh-pages/Xtreme-SunBlind/src';
 	this.game.load.crossOrigin = 'anonymous';
     //this.game.load.image('preloader_bar', 'images/preloader_bar.png');
   },
@@ -23,21 +23,21 @@ var PreloaderScene = {
     //this.load.setPreloadSprite(this.loadingBar);
 
     // TODO: load here the assets for the game
-    this.game.load.image('logo', './images/phaser.png');
+    this.game.load.image('logo', 'images/phaser.png');
     this.game.stage.backgroundColor = '#f1f';
-    this.game.load.spritesheet('player', './images/alientotal.png', 60, 57, 15);
-    this.game.load.spritesheet('plat0', './images/plat0.png', 64, 64, 3);
-    this.game.load.spritesheet('plat1', './images/plat1.png', 64, 64, 3);
-    this.game.load.spritesheet('plat2', './images/plat2.png', 64, 64, 3);
-    this.game.load.image('fond', './images/space.png');
-    this.game.load.image('enemigo', './images/juen.png');
-    this.game.load.image('perder', './images/lose.png');
-    this.game.load.image('energetica', './images/Energetica.png');
-    this.game.load.image('alcohol', './images/alcohol.png');
-    this.game.load.image('agua', './images/agua.png');
-    this.game.load.image('proteinas', './images/proteinas.png');
-    this.game.load.image('crabby', './images/crab.png');
-    this.game.load.image('fly', './images/fly.png');
+    this.game.load.spritesheet('player', 'images/alientotal.png', 60, 57, 15);
+    this.game.load.spritesheet('plat0', 'images/plat0.png', 64, 64, 3);
+    this.game.load.spritesheet('plat1', 'images/plat1.png', 64, 64, 3);
+    this.game.load.spritesheet('plat2', 'images/plat2.png', 64, 64, 3);
+    this.game.load.image('fond', 'images/space.png');
+    this.game.load.image('enemigo', 'images/juen.png');
+    this.game.load.image('perder', 'images/lose.png');
+    this.game.load.image('energetica', 'images/Energetica.png');
+    this.game.load.image('alcohol', 'images/alcohol.png');
+    this.game.load.image('agua', 'images/agua.png');
+    this.game.load.image('proteinas', 'images/proteinas.png');
+    this.game.load.image('crabby', 'images/crab.png');
+    this.game.load.image('fly', 'images/fly.png');
   },
 
   create: function () {
