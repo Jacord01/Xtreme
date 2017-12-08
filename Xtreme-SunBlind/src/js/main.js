e17402564d62ad8cc8abf743f8e794440be76ab0
Javier Cordero Calvo <jacord01@ucm.es>
2017-12-08 14:28:11 +0100

    Intento 2

--- Xtreme-SunBlind/src/js/main.js
+++ Xtreme-SunBlind/src/js/main.js
@@ -23,21 +23,21 @@ var PreloaderScene = {
     //this.load.setPreloadSprite(this.loadingBar);
 
     // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
+    this.game.load.image('logo', './images/phaser.png');
     this.game.stage.backgroundColor = '#f1f';
-    this.game.load.spritesheet('player', 'images/alientotal.png', 60, 57, 15);
-    this.game.load.spritesheet('plat0', 'images/plat0.png', 64, 64, 3);
-    this.game.load.spritesheet('plat1', 'images/plat1.png', 64, 64, 3);
-    this.game.load.spritesheet('plat2', 'images/plat2.png', 64, 64, 3);
-    this.game.load.image('fond', 'images/space.png');
-    this.game.load.image('enemigo', 'images/juen.png');
-    this.game.load.image('perder', 'images/lose.png');
-    this.game.load.image('energetica', 'images/Energetica.png');
-    this.game.load.image('alcohol', 'images/alcohol.png');
-    this.game.load.image('agua', 'images/agua.png');
-    this.game.load.image('proteinas', 'images/proteinas.png');
-    this.game.load.image('crabby', 'images/crab.png');
-    this.game.load.image('fly', 'images/fly.png');
+    this.game.load.spritesheet('player', './images/alientotal.png', 60, 57, 15);
+    this.game.load.spritesheet('plat0', './images/plat0.png', 64, 64, 3);
+    this.game.load.spritesheet('plat1', './images/plat1.png', 64, 64, 3);
+    this.game.load.spritesheet('plat2', './images/plat2.png', 64, 64, 3);
+    this.game.load.image('fond', './images/space.png');
+    this.game.load.image('enemigo', './images/juen.png');
+    this.game.load.image('perder', './images/lose.png');
+    this.game.load.image('energetica', './images/Energetica.png');
+    this.game.load.image('alcohol', './images/alcohol.png');
+    this.game.load.image('agua', './images/agua.png');
+    this.game.load.image('proteinas', './images/proteinas.png');
+    this.game.load.image('crabby', './images/crab.png');
+    this.game.load.image('fly', './images/fly.png');
   },
 
   create: function () {
