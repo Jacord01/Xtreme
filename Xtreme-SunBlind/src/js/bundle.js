453f62dece75682cf9073adaaa901d0b9859257e
Jacord01 <jacord01@ucm.es>
2018-01-30 17:34:20 +0100

    eliminado boton movil hasta que sea viable

--- Xtreme-SunBlind/src/js/bundle.js
+++ Xtreme-SunBlind/src/js/bundle.js
@@ -1968,12 +1968,12 @@ var menu = {
     punt.width = 150;
     punt.height = 60;
 
-    //Boton para movil
+  /*  //Boton para movil
     movil = juego.add.button(juego.world.centerX + 400, 300, 'button', movilClick, this, 2,1,0);
     movil.animations.add('button');
     movil.animations.play('button', 4, true );
     movil.width = 150;
-    movil.height = 60;
+    movil.height = 60;*/
  },
 };
 
